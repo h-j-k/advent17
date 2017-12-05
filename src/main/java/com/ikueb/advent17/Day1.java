e96f7325017d708389ba894a773e695322ac3216
h-j-k <han.junkwang@gmail.com>
2017-12-05 20:46:37 +0800

    Day 1

---
+++ src/main/java/com/ikueb/advent17/Day1.java
@@ -0,0 +1,34 @@
+package com.ikueb.advent17;
+
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
+import java.util.function.ToIntFunction;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class Day1 {
+
+    public static int compute(String value, ToIntFunction<int[]> function) {
+        return function.applyAsInt(Pattern.compile("")
+                .splitAsStream(value)
+                .mapToInt(Integer::parseInt)
+                .toArray());
+    }
+
+    public static int sum(int... values) {
+        return sum(values.length, i -> i + 1 == values.length ? 0 : i + 1,
+                (a, b) -> a == b ? a : 0, values);
+    }
+
+    public static int jumpSum(int... values) {
+        int half = values.length / 2;
+        return sum(half, i -> i + half, (a, b) -> a == b ? 2 * a : 0, values);
+    }
+
+    private static int sum(int end, IntUnaryOperator pair,
+                           IntBinaryOperator op, int... values) {
+        return IntStream.range(0, end)
+                .map(i -> op.applyAsInt(values[i], values[pair.applyAsInt(i)]))
+                .sum();
+    }
+}
