a80ad16e8cece2643d1bef7ef6ae7a93ddc90021
h-j-k <han.junkwang@gmail.com>
2017-12-07 23:25:37 +0800

    Day 7 part one

--- src/main/java/com/ikueb/advent17/Day7.java
+++ src/main/java/com/ikueb/advent17/Day7.java
@@ -1,9 +1,10 @@
 package com.ikueb.advent17;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 final class Day7 {
 
@@ -12,27 +13,63 @@ final class Day7 {
     }
 
     static String getBottomProgram(Collection<String> programs) {
-        return null;
+        Map<String, Program> map = programs.stream()
+                .map(Program::parse)
+                .collect(Collectors.toMap(Program::getName, Function.identity()));
+        map.values().forEach(p -> p.setChildren(map));
+        return map.entrySet().stream()
+                .filter(entry -> !entry.getValue().hasParent())
+                .findAny()
+                .map(Map.Entry::getKey)
+                .orElseThrow(
+                        () -> new UnexpectedOutcomeException("Should find a parent"));
     }
 
     private static final class Program {
 
+        private static final Pattern LINE_PARSER =
+                Pattern.compile("(?<name>[a-z]+) \\((?<weight>\\d+)\\)( -> (?<children>.+))?");
+        private static final Pattern CHILDREN_PARSER =
+                Pattern.compile(", ");
+
         private Program parent = null;
+        private final String name;
         private final int weight;
         private final Set<String> childrenNames;
         private final Set<Program> children = new HashSet<>();
 
-        Program(int weight, Set<String> childrenNames) {
+        Program(String name, int weight, Set<String> childrenNames) {
+            this.name = name;
             this.weight = weight;
             this.childrenNames = childrenNames;
         }
 
-        void setParent(Program parent) {
-            this.parent = parent;
+        void setChildren(Map<String, Program> map) {
+            childrenNames.stream().map(map::get).forEach(p -> {
+                p.parent = this;
+                children.add(p);
+            });
         }
 
-        void setChildren(Map<String, Program> map) {
-            childrenNames.stream().map(map::get).forEach(children::add);
+        String getName() {
+            return name;
+        }
+
+        boolean hasParent() {
+            return parent != null;
+        }
+
+        static Program parse(String line) {
+            Matcher matcher = LINE_PARSER.matcher(line);
+            if (!matcher.matches()) {
+                throw new UnexpectedOutcomeException("Unable to parse: " + line);
+            }
+            return new Program(
+                    matcher.group("name"),
+                    Integer.parseInt(matcher.group("weight")),
+                    CHILDREN_PARSER.splitAsStream(
+                            Objects.toString(matcher.group("children"), ""))
+                            .collect(Collectors.toSet()));
         }
     }
 }
