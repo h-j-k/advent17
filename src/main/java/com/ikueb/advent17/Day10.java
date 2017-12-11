7db5eeea98077eb6d8dc64313895d987d7a87a2a
h-j-k <han.junkwang@gmail.com>
2017-12-11 23:16:30 +0800

    Day 10 part one

--- src/main/java/com/ikueb/advent17/Day10.java
+++ src/main/java/com/ikueb/advent17/Day10.java
@@ -1,12 +1,31 @@
 package com.ikueb.advent17;
 
+import java.util.function.IntBinaryOperator;
+import java.util.stream.IntStream;
+
 final class Day10 {
 
     private Day10() {
         // empty
     }
 
-    static int knotAndMultiplyFirstTwo(int listLength, int[] lengths) {
-        return -1;
+    static int knotAndMultiplyFirstTwo(int listSize, int[] lengths) {
+        int[] input = IntStream.range(0, listSize).toArray();
+        IntBinaryOperator index = (a, b) -> a + b - listSize * ((a + b) / listSize);
+        for (int i = 0, skip = 0, current, start = 0;
+             i < lengths.length;
+             i++, start = index.applyAsInt(start, current + skip), skip++) {
+            current = lengths[i];
+            int t = start;
+            int[] selection = IntStream.range(0, current)
+                    .map(j -> index.applyAsInt(t, j))
+                    .toArray();
+            for (int j = 0; j < current / 2; j++) {
+                int temp = input[selection[j]];
+                input[selection[j]] = input[selection[current - j - 1]];
+                input[selection[current - j - 1]] = temp;
+            }
+        }
+        return input[0] * input[1];
     }
 }
--- src/test/java/com/ikueb/advent17/Day10Test.java
+++ src/test/java/com/ikueb/advent17/Day10Test.java
@@ -9,9 +9,10 @@ import static org.hamcrest.Matchers.equalTo;
 public class Day10Test {
 
     @Test
-    public void testExample() {
-        assertThat(knotAndMultiplyFirstTwo(5,
-                new int[]{3, 4, 1, 5}), equalTo(12));
+    public void testPartOne() {
+        assertThat(knotAndMultiplyFirstTwo(5, new int[]{3, 4, 1, 5}), equalTo(12));
+        assertThat(knotAndMultiplyFirstTwo(256, LENGTHS), equalTo(50850));
     }
 
+    private static final int[] LENGTHS = {18, 1, 0, 161, 255, 137, 254, 252, 14, 95, 165, 33, 181, 168, 2, 188};
 }
