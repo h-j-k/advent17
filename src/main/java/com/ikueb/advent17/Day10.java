938e4e7ea208272a2824341612a26a4d4caf7bd6
h-j-k <han.junkwang@gmail.com>
2017-12-12 00:25:07 +0800

    Day 10 part two

--- src/main/java/com/ikueb/advent17/Day10.java
+++ src/main/java/com/ikueb/advent17/Day10.java
@@ -1,6 +1,8 @@
 package com.ikueb.advent17;
 
+import java.util.Arrays;
 import java.util.function.IntBinaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 final class Day10 {
@@ -10,9 +12,40 @@ final class Day10 {
     }
 
     static int knotAndMultiplyFirstTwo(int listSize, int[] lengths) {
-        int[] input = IntStream.range(0, listSize).toArray();
+        Payload payload = knot(new Payload(listSize), lengths);
+        return payload.copyResult()[0] * payload.copyResult()[1];
+    }
+
+    static String hash(String value) {
+        int[] lengths = IntStream.concat(
+                value.chars(),
+                IntStream.of(17, 31, 73, 47, 23)).toArray();
+        Payload payload = IntStream.range(0, 64).boxed().reduce(
+                new Payload(256),
+                (r, i) -> knot(r, lengths),
+                (a, b) -> {
+                    throw new UnsupportedOperationException();
+                });
+        int[] result = payload.copyResult();
+        return IntStream.range(0, 16)
+                .map(i -> IntStream.range(i * 16, (i + 1) * 16)
+                        .mapToObj(j -> result[j])
+                        .reduce(0, (a, b) -> a ^ b))
+                .mapToObj(Day10::paddedHex)
+                .collect(Collectors.joining(""));
+    }
+
+    private static String paddedHex(int i) {
+        return i < 16 ? "0" + Integer.toHexString(i) : Integer.toHexString(i);
+    }
+
+    private static Payload knot(Payload payload, int[] lengths) {
+        int[] input = payload.copyResult();
+        int start = payload.getStart();
+        int skip = payload.getSkip();
+        int listSize = input.length;
         IntBinaryOperator index = (a, b) -> a + b - listSize * ((a + b) / listSize);
-        for (int i = 0, skip = 0, current, start = 0;
+        for (int i = 0, current;
              i < lengths.length;
              i++, start = index.applyAsInt(start, current + skip), skip++) {
             current = lengths[i];
@@ -26,6 +59,34 @@ final class Day10 {
                 input[selection[current - j - 1]] = temp;
             }
         }
-        return input[0] * input[1];
+        return new Payload(start, skip, input);
+    }
+
+    static final class Payload {
+        private final int start;
+        private final int skip;
+        private final int[] result;
+
+        Payload(int listSize) {
+            this(0, 0, IntStream.range(0, listSize).toArray());
+        }
+
+        Payload(int start, int skip, int[] result) {
+            this.start = start;
+            this.skip = skip;
+            this.result = Arrays.copyOf(result, result.length);
+        }
+
+        int getStart() {
+            return start;
+        }
+
+        int getSkip() {
+            return skip;
+        }
+
+        int[] copyResult() {
+            return Arrays.stream(result).toArray();
+        }
     }
 }
--- src/test/java/com/ikueb/advent17/Day10Test.java
+++ src/test/java/com/ikueb/advent17/Day10Test.java
@@ -1,7 +1,14 @@
 package com.ikueb.advent17;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.ikueb.advent17.Day10.hash;
 import static com.ikueb.advent17.Day10.knotAndMultiplyFirstTwo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -12,6 +19,28 @@ public class Day10Test {
     public void testPartOne() {
         assertThat(knotAndMultiplyFirstTwo(5, new int[]{3, 4, 1, 5}), equalTo(12));
         assertThat(knotAndMultiplyFirstTwo(256, LENGTHS), equalTo(46600));
+    }
+
+    @DataProvider(name = "partTwo")
+    public Iterator<Object[]> getTestCases() {
+        return Stream.of(
+                with("", "a2582a3a0e66e6e86e3812dcb672a272"),
+                with("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd"),
+                with("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"),
+                with("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e"),
+                with(Arrays.stream(LENGTHS)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(",")), "23234babdc6afa036749cfa9b597de1b")
+        ).iterator();
+    }
+
+    private static Object[] with(String input, String hash) {
+        return new Object[]{input, hash};
+    }
+
+    @Test(dataProvider = "partTwo")
+    public void testPartTwo(String input, String hash) {
+        assertThat(hash(input), equalTo(hash));
     }
 
     private static final int[] LENGTHS = {18, 1, 0, 161, 255, 137, 254, 252, 14, 95, 165, 33, 181, 168, 2, 188};
