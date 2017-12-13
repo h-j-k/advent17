9d82618eb7367ad01061de8b0b794b0cf8d7883e
h-j-k <han.junkwang@gmail.com>
2017-12-13 23:35:06 +0800

    Day 12 part one

--- src/main/java/com/ikueb/advent17/Day12.java
+++ src/main/java/com/ikueb/advent17/Day12.java
@@ -1,6 +1,11 @@
 package com.ikueb.advent17;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 final class Day12 {
 
@@ -8,7 +13,70 @@ final class Day12 {
         // empty
     }
 
-    static int getGroupCount(List<String> list, int groupId) {
-        return -1;
+    static int getGroupCount(Collection<String> programs, int groupId) {
+        Map<Integer, Program> map = generateMap(programs);
+        Program target = map.get(groupId);
+        Set<Program> seen = new HashSet<>(Collections.singleton(target));
+        aggregate(target.streamTargetsExcluding(seen), seen);
+        return seen.size();
+    }
+
+    private static Map<Integer, Program> generateMap(Collection<String> programs) {
+        Map<Integer, Program> map = programs.stream()
+                .map(Program::parse)
+                .collect(Collectors.toMap(Program::getId, Function.identity()));
+        map.values().forEach(p -> p.setTargets(map));
+        return map;
+    }
+
+    private static void aggregate(Stream<Program> stream, Set<Program> seen) {
+        stream.peek(seen::add)
+                .forEach(v -> aggregate(v.streamTargetsExcluding(seen), seen));
+    }
+
+    static final class Program {
+
+        private static final Pattern LINE_PARSER = Pattern.compile(
+                "(?<id>[0-9]+) <-> (?<target>.+)");
+        private static final Pattern TARGET_PARSER = Pattern.compile(", ");
+
+        private final int id;
+        private final Set<Integer> targetNames;
+        private final Set<Program> others = new HashSet<>();
+        private final Set<Program> targets = new HashSet<>();
+
+        Program(int id, Set<Integer> targetNames) {
+            this.id = id;
+            this.targetNames = targetNames;
+        }
+
+        void setTargets(Map<Integer, Program> map) {
+            targetNames.stream().map(map::get).forEach(p -> {
+                p.others.add(this);
+                targets.add(p);
+            });
+        }
+
+        int getId() {
+            return id;
+        }
+
+        Stream<Program> streamTargetsExcluding(Set<Program> seen) {
+            return targets.stream()
+                    .filter(program -> !seen.contains(program));
+        }
+
+        static Program parse(String line) {
+            Matcher matcher = LINE_PARSER.matcher(line);
+            if (!matcher.matches()) {
+                throw new UnexpectedException("Unable to parse: " + line);
+            }
+            return new Program(
+                    Integer.parseInt(matcher.group("id")),
+                    TARGET_PARSER.splitAsStream(matcher.group("target"))
+                            .map(Integer::valueOf)
+                            .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                                    Collections::unmodifiableSet)));
+        }
     }
 }
