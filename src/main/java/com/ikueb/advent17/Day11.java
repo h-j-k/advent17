9ff2f00ad9f07fdd4f8f8800ecfe4bbfc5adba6f
h-j-k <han.junkwang@gmail.com>
2017-12-13 22:50:03 +0800

    Day 11 part two

--- src/main/java/com/ikueb/advent17/Day11.java
+++ src/main/java/com/ikueb/advent17/Day11.java
@@ -12,23 +12,32 @@ final class Day11 {
     }
 
     static int getFewestStepsTo(String steps) {
+        return traceSteps(steps).countStepsFromOrigin();
+    }
+
+    static int getFurthest(String steps) {
+        return traceSteps(steps).getFurthest();
+    }
+
+    private static Counter traceSteps(String steps) {
         return SPLITTER.splitAsStream(steps)
                 .map(step -> Cardinal.valueOf(step.toUpperCase()))
                 .reduce(new Counter(), Counter::moveTo,
-                        (a, b) -> { throw new UnsupportedOperationException(); })
-                .countStepsFromOrigin();
+                        (a, b) -> { throw new UnsupportedOperationException(); });
     }
 
     private static final class Counter {
 
-        private int x;
-        private int y;
-        private int z;
+        private int x = 0;
+        private int y = 0;
+        private int z = 0;
+        private int furthest = 0;
 
         Counter moveTo(Cardinal cardinal) {
             x += cardinal.x;
             y += cardinal.y;
             z += cardinal.z;
+            furthest = Math.max(furthest, countStepsFromOrigin());
             return this;
         }
 
@@ -37,6 +46,10 @@ final class Day11 {
                     .map(Math::abs)
                     .max()
                     .orElse(0);
+        }
+
+        int getFurthest() {
+            return furthest;
         }
     }
 
