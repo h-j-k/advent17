5ef0eb49ffd4a39b8288424deee1c0a10dd5c149
h-j-k <han.junkwang@gmail.com>
2017-12-14 23:24:08 +0800

    Day 13 part one

--- src/main/java/com/ikueb/advent17/Day13.java
+++ src/main/java/com/ikueb/advent17/Day13.java
@@ -1,6 +1,11 @@
 package com.ikueb.advent17;
 
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 final class Day13 {
 
@@ -9,6 +14,43 @@ final class Day13 {
     }
 
     static int getTripSeverity(List<String> levels) {
-        return -1;
+        return levels.stream()
+                .map(Layer::parse)
+                .mapToInt(Layer::getSeverity)
+                .sum();
+    }
+
+    static final class Layer {
+
+        private static final Pattern PARSER = Pattern.compile(
+                "(?<position>[0-9]+): (?<level>[0-9]+)");
+
+        private final int position;
+        private final int level;
+
+        private Layer(int position, int level) {
+            this.position = position;
+            this.level = level;
+        }
+
+        private Set<Integer> getTimesAtTop() {
+            return IntStream.rangeClosed(0, position)
+                    .filter(i -> level == 1 || i % (2 * (level - 1)) == 0)
+                    .boxed()
+                    .collect(Collectors.toSet());
+        }
+
+        int getSeverity() {
+            return getTimesAtTop().contains(position) ? position * level : 0;
+        }
+
+        static Layer parse(String layer) {
+            Matcher matcher = PARSER.matcher(layer);
+            if (!matcher.matches()) {
+                throw new UnexpectedException("Unable to parse: " + layer);
+            }
+            return new Layer(Integer.parseInt(matcher.group("position")),
+                    Integer.parseInt(matcher.group("level")));
+        }
     }
 }
--- src/test/java/com/ikueb/advent17/Day13Test.java
+++ src/test/java/com/ikueb/advent17/Day13Test.java
@@ -1,5 +1,6 @@
 package com.ikueb.advent17;
 
+import com.ikueb.advent17.Day13.Layer;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
@@ -14,6 +15,13 @@ public class Day13Test {
     @Test
     public void testTripSeverity() {
         assertThat(getTripSeverity(EXAMPLE), equalTo(24));
+        assertThat(getTripSeverity(LEVELS), equalTo(1504));
+    }
+
+    @Test(expectedExceptions = UnexpectedException.class,
+            expectedExceptionsMessageRegExp = "^Unable to parse: malformed_line")
+    public void testUnableToParseMalformedLine() {
+        Layer.parse("malformed_line");
     }
 
     private static final List<String> EXAMPLE = Arrays.asList(
@@ -21,5 +29,51 @@ public class Day13Test {
             "1: 2",
             "4: 4",
             "6: 4"
+    );
+
+    private static final List<String> LEVELS = Arrays.asList(
+            "0: 3",
+            "1: 2",
+            "2: 4",
+            "4: 6",
+            "6: 4",
+            "8: 6",
+            "10: 5",
+            "12: 6",
+            "14: 9",
+            "16: 6",
+            "18: 8",
+            "20: 8",
+            "22: 8",
+            "24: 8",
+            "26: 8",
+            "28: 8",
+            "30: 12",
+            "32: 14",
+            "34: 10",
+            "36: 12",
+            "38: 12",
+            "40: 10",
+            "42: 12",
+            "44: 12",
+            "46: 12",
+            "48: 12",
+            "50: 12",
+            "52: 14",
+            "54: 14",
+            "56: 12",
+            "62: 12",
+            "64: 14",
+            "66: 14",
+            "68: 14",
+            "70: 17",
+            "72: 14",
+            "74: 14",
+            "76: 14",
+            "82: 14",
+            "86: 18",
+            "88: 14",
+            "96: 14",
+            "98: 44"
     );
 }
