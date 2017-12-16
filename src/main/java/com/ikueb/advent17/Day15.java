8feb0353ef9321255bc6145ddd53704919ee05c3
h-j-k <han.junkwang@gmail.com>
2017-12-16 16:06:00 +0800

    Day 15 part two

--- src/main/java/com/ikueb/advent17/Day15.java
+++ src/main/java/com/ikueb/advent17/Day15.java
@@ -1,7 +1,7 @@
 package com.ikueb.advent17;
 
-import java.util.function.IntBinaryOperator;
-import java.util.function.IntUnaryOperator;
+import java.util.function.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 final class Day15 {
@@ -10,25 +10,22 @@ final class Day15 {
         // empty
     }
 
-    static long getMatchingLowest16Digits(int aSeed, int bSeed) {
-        return Stream.iterate(GeneratorPair.of(aSeed, bSeed), GeneratorPair::next)
-                .limit(40_000_000)
+    static long getMatchingLowest16Digits(int aSeed, int bSeed,
+                                          UnaryOperator<GeneratorPair> generator,
+                                          long limit) {
+        return Stream.iterate(GeneratorPair.of(aSeed, bSeed), generator)
+                .limit(limit)
                 .filter(GeneratorPair::matches)
                 .count();
     }
 
-    private static final class GeneratorPair {
+    static final class GeneratorPair {
 
-        private static final int A_MULTIPLIER = 16807;
-        private static final int B_MULTIPLIER = 48271;
         private static final IntBinaryOperator OP =
                 (x, y) -> (int) (((long) x * y) % Integer.MAX_VALUE);
-        private static final IntUnaryOperator A_OP =
-                a -> OP.applyAsInt(a, A_MULTIPLIER);
-        private static final IntUnaryOperator B_OP =
-                b -> OP.applyAsInt(b, B_MULTIPLIER);
-        private static final IntUnaryOperator EXTRACTOR =
-                x -> x % 65536;
+        private static final IntUnaryOperator A_OP = i -> OP.applyAsInt(i, 16807);
+        private static final IntUnaryOperator B_OP = i -> OP.applyAsInt(i, 48271);
+        private static final IntUnaryOperator EXTRACTOR = x -> x % 65536;
 
         private final int a;
         private final int b;
@@ -42,17 +39,26 @@ final class Day15 {
             return of(a, b);
         }
 
+        GeneratorPair next(IntPredicate aPredicate, IntPredicate bPredicate) {
+            return of(findFirst(IntStream.iterate(a, A_OP).filter(aPredicate)),
+                    findFirst(IntStream.iterate(b, B_OP).filter(bPredicate)));
+        }
+
         boolean matches() {
             return EXTRACTOR.applyAsInt(a) == EXTRACTOR.applyAsInt(b);
         }
 
         static GeneratorPair of(int a, int b) {
-            return new GeneratorPair(A_OP.applyAsInt(a), B_OP.applyAsInt(b));
+            return of(() -> A_OP.applyAsInt(a), () -> B_OP.applyAsInt(b));
         }
 
-        @Override
-        public String toString() {
-            return a + ", " + b;
+        static GeneratorPair of(IntSupplier aSupplier, IntSupplier bSupplier) {
+            return new GeneratorPair(aSupplier.getAsInt(), bSupplier.getAsInt());
+        }
+
+        private static IntSupplier findFirst(IntStream stream) {
+            return () -> stream.skip(1).findFirst()
+                    .orElseThrow(() -> new UnexpectedException("Expecting a value."));
         }
     }
 }
--- src/test/java/com/ikueb/advent17/Day15Test.java
+++ src/test/java/com/ikueb/advent17/Day15Test.java
@@ -9,9 +9,18 @@ import static org.hamcrest.Matchers.equalTo;
 public class Day15Test {
 
     @Test
-    public void testMatchingLowest16Digits() {
-        assertThat(getMatchingLowest16Digits(65, 8921), equalTo(588L));
-        assertThat(getMatchingLowest16Digits(699, 124), equalTo(600L));
+    public void testPartOne() {
+        assertThat(getMatchingLowest16Digits(65, 8921,
+                Day15.GeneratorPair::next, 40_000_000), equalTo(588L));
+        assertThat(getMatchingLowest16Digits(699, 124,
+                Day15.GeneratorPair::next, 40_000_000), equalTo(600L));
     }
 
+    @Test
+    public void testPartTwo() {
+        assertThat(getMatchingLowest16Digits(65, 8921,
+                v -> v.next(a -> a % 4 == 0, b -> b % 8 == 0), 5_000_000), equalTo(309L));
+        assertThat(getMatchingLowest16Digits(699, 124,
+                v -> v.next(a -> a % 4 == 0, b -> b % 8 == 0), 5_000_000), equalTo(313L));
+    }
 }
