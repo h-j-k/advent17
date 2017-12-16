e33f9f0e88f33ee56aa2a4bb7c332b38d255202d
h-j-k <han.junkwang@gmail.com>
2017-12-16 09:43:33 +0800

    Day 15

---
+++ src/main/java/com/ikueb/advent17/Day15.java
@@ -0,0 +1,12 @@
+package com.ikueb.advent17;
+
+final class Day15 {
+
+    private Day15() {
+        // empty
+    }
+
+    static int getMatchingLowest16Digits(int aSeed, int bSeed) {
+        return -1;
+    }
+}
---
+++ src/test/java/com/ikueb/advent17/Day15Test.java
@@ -0,0 +1,16 @@
+package com.ikueb.advent17;
+
+import org.testng.annotations.Test;
+
+import static com.ikueb.advent17.Day15.getMatchingLowest16Digits;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Day15Test {
+
+    @Test
+    public void testMatchingLowest16Digits() {
+        assertThat(getMatchingLowest16Digits(65, 8921), equalTo(588));
+    }
+
+}
