4252056588f60a5fac0934e2b599eb9408717ba4
h-j-k <han.junkwang@gmail.com>
2017-12-17 15:50:08 +0800

    Day 17

---
+++ src/main/java/com/ikueb/advent17/Day17.java
@@ -0,0 +1,12 @@
+package com.ikueb.advent17;
+
+final class Day17 {
+
+    private Day17() {
+        // empty
+    }
+
+    static int getNextAfter2017WithSteps(int steps) {
+        return -1;
+    }
+}
---
+++ src/test/java/com/ikueb/advent17/Day17Test.java
@@ -0,0 +1,16 @@
+package com.ikueb.advent17;
+
+import org.testng.annotations.Test;
+
+import static com.ikueb.advent17.Day17.getNextAfter2017WithSteps;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Day17Test {
+
+    @Test
+    public void testGetNextValueAfter2017() {
+        assertThat(getNextAfter2017WithSteps(3), equalTo(638));
+    }
+
+}
