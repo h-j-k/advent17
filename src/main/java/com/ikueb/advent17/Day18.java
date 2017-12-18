eb28d1feef956dce398c729be33d67775ec4f42a
h-j-k <han.junkwang@gmail.com>
2017-12-18 23:18:19 +0800

    Day 18 part one

--- src/main/java/com/ikueb/advent17/Day18.java
+++ src/main/java/com/ikueb/advent17/Day18.java
@@ -1,6 +1,9 @@
 package com.ikueb.advent17;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 final class Day18 {
 
@@ -8,7 +11,96 @@ final class Day18 {
         // empty
     }
 
-    static int recoverFrequency(List<String> instructions) {
-        return -1;
+    static long recoverFrequency(List<String> instructions) {
+        long lastPlayed = -1;
+        Map<Character, Long> map = new HashMap<>();
+        List<String> temp = new ArrayList<>(instructions);
+        Result result;
+        for (int i = 0; ; i += result.jumpToNextInstruction()) {
+            result = Instruction.compute(map, temp.get(i));
+            if (result.isPlayed()) {
+                lastPlayed = result.getResult();
+            } else if (result.isRecovered()) {
+                return lastPlayed;
+            }
+        }
+    }
+
+    private static final class Result {
+        private final Instruction instruction;
+        private final Long result;
+
+        private Result(Instruction instruction, Long result) {
+            this.instruction = instruction;
+            this.result = result;
+        }
+
+        long getResult() {
+            return Optional.ofNullable(result).orElseThrow(
+                    () -> new UnexpectedException("Expecting a result."));
+        }
+
+        boolean isPlayed() {
+            return instruction == Instruction.SND;
+        }
+
+        boolean isRecovered() {
+            return instruction == Instruction.RCV && result != null;
+        }
+
+        int jumpToNextInstruction() {
+            return instruction == Instruction.JGZ && result != null
+                    ? result.intValue() : 1;
+        }
+    }
+
+    private enum Instruction
+            implements BiFunction<Map<Character, Long>, String, Long> {
+        SND((map, i) -> map.get(getRegister(i))),
+        SET((map, i) -> map.put(getRegister(i), getValue(map, i))),
+        ADD((map, i) -> map.merge(getRegister(i), getValue(map, i), Math::addExact)),
+        MUL((map, i) -> map.compute(getRegister(i),
+                (k, v) -> v == null ? 0 : v * getValue(map, i))),
+        MOD((map, i) -> map.compute(getRegister(i),
+                (k, v) -> v == null ? 0 : v % getValue(map, i))),
+        RCV((map, i) -> Optional.ofNullable(map.get(getRegister(i)))
+                .filter(v -> v != 0).orElse(null)),
+        JGZ((map, i) -> Optional.ofNullable(map.get(getRegister(i)))
+                .filter(v -> v > 0).map(v -> getValue(map, i)).orElse(null));
+
+        private static final Map<String, Instruction> INSTRUCTIONS = Arrays.stream(values())
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toMap(Instruction::getKey, Function.identity()),
+                        Collections::unmodifiableMap));
+
+        private final BiFunction<Map<Character, Long>, String, Long> op;
+
+        Instruction(BiFunction<Map<Character, Long>, String, Long> op) {
+            this.op = op;
+        }
+
+        @Override
+        public Long apply(Map<Character, Long> map, String instruction) {
+            return op.apply(map, instruction);
+        }
+
+        private String getKey() {
+            return name().toLowerCase();
+        }
+
+        private static char getRegister(String instruction) {
+            return instruction.charAt(0);
+        }
+
+        private static long getValue(Map<Character, Long> map, String instruction) {
+            return Character.isAlphabetic(instruction.charAt(2))
+                    ? map.getOrDefault(instruction.charAt(2), 0L)
+                    : Long.parseLong(instruction.substring(2));
+        }
+
+        static Result compute(Map<Character, Long> map, String instruction) {
+            Instruction current = INSTRUCTIONS.get(instruction.substring(0, 3));
+            return new Result(current, current.apply(map, instruction.substring(4)));
+        }
     }
 }
--- src/test/java/com/ikueb/advent17/Day18Test.java
+++ src/test/java/com/ikueb/advent17/Day18Test.java
@@ -13,7 +13,8 @@ public class Day18Test {
 
     @Test
     public void testRecoverFrequency() {
-        assertThat(recoverFrequency(EXAMPLE), equalTo(4));
+        assertThat(recoverFrequency(EXAMPLE), equalTo(4L));
+        assertThat(recoverFrequency(INSTRUCTIONS), equalTo(9423L));
     }
 
     private static final List<String> EXAMPLE = Arrays.asList(
@@ -27,6 +28,50 @@ public class Day18Test {
             "jgz a -1",
             "set a 1",
             "jgz a -2"
+    );
+
+    private static final List<String> INSTRUCTIONS = Arrays.asList(
+            "set i 31",
+            "set a 1",
+            "mul p 17",
+            "jgz p p",
+            "mul a 2",
+            "add i -1",
+            "jgz i -2",
+            "add a -1",
+            "set i 127",
+            "set p 622",
+            "mul p 8505",
+            "mod p a",
+            "mul p 129749",
+            "add p 12345",
+            "mod p a",
+            "set b p",
+            "mod b 10000",
+            "snd b",
+            "add i -1",
+            "jgz i -9",
+            "jgz a 3",
+            "rcv b",
+            "jgz b -1",
+            "set f 0",
+            "set i 126",
+            "rcv a",
+            "rcv b",
+            "set p a",
+            "mul p -1",
+            "add p b",
+            "jgz p 4",
+            "snd a",
+            "set a b",
+            "jgz 1 3",
+            "snd b",
+            "set f 1",
+            "add i -1",
+            "jgz i -11",
+            "snd a",
+            "jgz f -16",
+            "jgz a -19"
     );
 
 }
