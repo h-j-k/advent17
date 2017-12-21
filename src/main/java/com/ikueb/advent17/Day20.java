7c8d0de8836e9715e7b99205f9fa930abf3acfd6
h-j-k <han.junkwang@gmail.com>
2017-12-21 22:33:55 +0800

    Day 20

--- src/main/java/com/ikueb/advent17/Day20.java
+++ src/main/java/com/ikueb/advent17/Day20.java
@@ -1,8 +1,14 @@
 package com.ikueb.advent17;
 
+import java.util.List;
+
 final class Day20 {
 
     private Day20() {
         // empty
+    }
+
+    static int getClosestParticleToOrigin(List<String> particles) {
+        return -1;
     }
 }
