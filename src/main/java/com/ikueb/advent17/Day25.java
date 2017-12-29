8813772635a65ad8fb7570482d7e595ac2a0e4ba
h-j-k <han.junkwang@gmail.com>
2017-12-29 10:10:22 +0800

    Day 25 refactoring.

--- src/main/java/com/ikueb/advent17/Day25.java
+++ src/main/java/com/ikueb/advent17/Day25.java
@@ -1,8 +1,11 @@
 package com.ikueb.advent17;
 
 import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.BiFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 final class Day25 {
 
@@ -15,17 +18,18 @@ final class Day25 {
 
     static long getDiagnosticChecksum(List<String> inputs) {
         Iterator<String> iterator = inputs.iterator();
-        char currentState = getCharacter(iterator.next());
+        char stateName = getCharacter(iterator.next());
         int iterations = getNumber(iterator.next());
         Map<Character, State> states = getStateMap(iterator);
         Map<Integer, Integer> results = new HashMap<>(Collections.singletonMap(0, 0));
-        int current = 0;
+        int index = 0;
         for (int i = 0; i < iterations; i++) {
-            Step step = states.get(currentState).getStepFor(results.get(current));
-            results.put(current, step.newValue);
-            current += step.shiftBy;
-            results.putIfAbsent(current, 0);
-            currentState = step.nextState;
+            State current = states.get(stateName);
+            int value = results.getOrDefault(index, 0);
+            stateName = current.nextStateName(value);
+            int shiftBy = current.shiftBy(value);
+            results.compute(index, current);
+            index += shiftBy;
         }
         return results.values().stream().filter(i -> i == 1).count();
     }
@@ -34,22 +38,18 @@ final class Day25 {
         Map<Character, State> map = new HashMap<>();
         while (iterator.hasNext()) {
             iterator.next();
-            char name = getCharacter(iterator.next());
-            iterator.next();
-            Step ifZero = Step.create(iterator);
-            iterator.next();
-            Step ifOne = Step.create(iterator);
-            map.put(name, new State(ifZero, ifOne));
+            map.put(getCharacter(iterator.next()),
+                    new State(Step.create(iterator), Step.create(iterator)));
         }
         return map;
     }
 
-    private static int getNumber(String input) {
-        return Integer.parseInt(getValue(input));
-    }
-
     private static char getCharacter(String input) {
         return getValue(input).charAt(0);
+    }
+
+    private static int getNumber(String input) {
+        return Integer.parseInt(getValue(input));
     }
 
     private static String getValue(String input) {
@@ -63,33 +63,47 @@ final class Day25 {
     private static final class Step {
         final int newValue;
         final int shiftBy;
-        final char nextState;
+        final char nextStateName;
 
-        private Step(int newValue, int shiftBy, char nextState) {
+        private Step(int newValue, int shiftBy, char nextStateName) {
             this.newValue = newValue;
             this.shiftBy = shiftBy;
-            this.nextState = nextState;
+            this.nextStateName = nextStateName;
         }
 
-        static Step create(Iterator<String> iterator) {
-            return new Step(
+        static Map<Integer, Step> create(Iterator<String> iterator) {
+            return Collections.singletonMap(
                     getNumber(iterator.next()),
-                    getValue(iterator.next()).equals("left") ? -1 : 1,
-                    getCharacter(iterator.next()));
+                    new Step(
+                            getNumber(iterator.next()),
+                            getValue(iterator.next()).equals("left") ? -1 : 1,
+                            getCharacter(iterator.next())));
         }
     }
 
-    private static final class State {
-        private final Step ifZero;
-        private final Step ifOne;
+    private static final class State implements BiFunction<Integer, Integer, Integer> {
+        private final Map<Integer, Step> conditions;
 
-        State(Step ifZero, Step ifOne) {
-            this.ifZero = ifZero;
-            this.ifOne = ifOne;
+        @SafeVarargs
+        State(Map<Integer, Step>... conditions) {
+            this.conditions = Arrays.stream(conditions)
+                    .flatMap(m -> m.entrySet().stream())
+                    .collect(Collectors.collectingAndThen(
+                            Collectors.toMap(Entry::getKey, Entry::getValue),
+                            Collections::unmodifiableMap));
         }
 
-        Step getStepFor(int current) {
-            return current == 0 ? ifZero : ifOne;
+        int shiftBy(int value) {
+            return conditions.get(value).shiftBy;
+        }
+
+        char nextStateName(int value) {
+            return conditions.get(value).nextStateName;
+        }
+
+        @Override
+        public Integer apply(Integer key, Integer value) {
+            return conditions.get(value == null ? 0 : value).newValue;
         }
     }
 }
